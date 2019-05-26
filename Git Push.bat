685acef751decec0556d59563013e984bd2f2807
user <you@example.com>
2019-05-26 09:22:59 +0200

    latest push from windows

--- Git Push.bat
+++ Git Push.bat
@@ -1,9 +1,9 @@
 
 echo off
 
-SET nom_Projet_Git=serverlessFulloolchainJava
-SET URL_Projet_Git=https://github.com/maddoudou22/serverlessFulloolchainJava.git
-SET Repertoire_Local_Projet=C:\awsLambda\java\serverlessFullToolchainJava
+SET nom_Projet_Git=smalltestproject
+SET URL_Projet_Git=https://github.com/maddoudou22/smalltestproject.git
+SET Repertoire_Local_Projet=C:\awsLambda\java\smalltestproject
 
 cd %Repertoire_Local_Projet%
 
